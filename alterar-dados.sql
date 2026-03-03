e419597ba9a2bfe7332669d62cf671896f941023
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 14:06:27 +0000

    adicao de comando onde é possivel atualizar o titulo de um colecionavel

--- alterar-dados.sql
+++ alterar-dados.sql
@@ -1,3 +1,5 @@
+use treasureorganizer;
+
 -- script de alteracao de dados
 
 -- mostrar lista de desejos
@@ -14,7 +16,6 @@ INSERT INTO Colecao (id_usuario, id_colecionavel, data_aquisicao)
 VALUES (1, 19, CURDATE());
 
 -- remover item colecionavel da lista de desejos
-
 DELETE FROM Lista_Desejos
 WHERE id_usuario = 1
 AND id_colecionavel = 19;
@@ -34,3 +35,15 @@ FROM Colecao
 JOIN Colecionavel 
 ON Colecao.id_colecionavel = Colecionavel.id
 WHERE Colecao.id_usuario = 1;
+
+-- atualiza o titulo de um livro
+UPDATE Colecionavel 
+SET titulo = 'Throne of Glass'
+WHERE id = 4;
+
+-- verifica se a alteracao foi realizada
+SELECT Colecionavel.id, Colecionavel.titulo
+FROM Colecionavel
+JOIN Colecao 
+ON Colecionavel.id = Colecao.id_colecionavel
+WHERE Colecionavel.id = 4;
