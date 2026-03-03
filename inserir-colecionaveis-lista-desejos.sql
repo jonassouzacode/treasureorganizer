3938183653c32b3e8fa8dad8e982f5004c7e6c1d
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 14:02:27 +0000

    reformulacao das tabelas para incluir "last insert id" e pequenos erros de sintaxe corrigidos em relacao aos comentarios

--- inserir-colecionaveis-lista-desejos.sql
+++ inserir-colecionaveis-lista-desejos.sql
@@ -1,41 +1,47 @@
---inserir colecionaveis desejados 
+use treasureorganizer;
 
---19
-INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) VALUES 
-('Livro', 'The Glass Horizon', '2016', 1, 1);
-INSERT INTO Editora (nome) VALUES
-('Harper Collins'); --6
-INSERT INTO Autor (nome) VALUES
-('Michael R. Whitaker'); --8
-INSERT INTO Livro (id_colecionavel, isbn, id_tipo_encadernacao, id_editora) VALUES 
-(19, '9780062458719', 2, 6); 
-INSERT INTO Livros_Autor (isbn, id_autor) VALUES 
-('9780062458719', 8);
-INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) VALUES 
-(1, 19, '2026-01-17');
+-- inserir colecionaveis desejados 
 
---20
-INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) VALUES 
-('Album', 'Frozen Signals', '2018', 11, 1); 
-INSERT INTO Album (id_colecionavel, numero_catalogo, ean_upc, tipo_album, id_gravadora) VALUES 
-(20, 'COL-CD-1834', '190758341223', 'CD', 3); 
-INSERT INTO Artista (nome) VALUES
-('Artic Veins'); --9
-INSERT INTO Album_Artista (id_colecionavel, id_artista) VALUES 
-(20, 9); 
-INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) VALUES 
-(1, 20, '2022-05-23');
+-- 19
+INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) 
+VALUES ('Livro', 'The Glass Horizon', '2016', 1, 1);
+SET @id_colecionavel = LAST_INSERT_ID();
+INSERT INTO Editora (nome) VALUES ('Harper Collins'); -- 6
+SET @id_editora = LAST_INSERT_ID();
+INSERT INTO Autor (nome) VALUES ('Michael R. Whitaker'); -- 8
+SET @id_autor = LAST_INSERT_ID();
+INSERT INTO Livro (id_colecionavel, isbn, id_tipo_encadernacao, id_editora) 
+VALUES (@id_colecionavel, '9780062458719', 2, @id_editora);
+INSERT INTO Livros_Autor (isbn, id_autor) 
+VALUES ('9780062458719', @id_autor);
+INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) 
+VALUES (1, @id_colecionavel, '2026-01-17');
 
---21
-INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) VALUES 
-('Album', 'Wildfire Heart', '2021', 13, 1); 
-INSERT INTO Gravadora (nome) VALUES
-('Capitol Records'); --9
-INSERT INTO Album (id_colecionavel, numero_catalogo, ean_upc, tipo_album, id_gravadora) VALUES 
-(21, 'CAP-CD-2147', '602438765432', 'CD', 9); 
-INSERT INTO Artista (nome) VALUES
-('Valerie Monroe'); --10
-INSERT INTO Album_Artista (id_colecionavel, id_artista) VALUES 
-(21, 10); 
-INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) VALUES 
-(1, 21, '2025-03-31');
+-- 20
+INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) 
+VALUES ('Album', 'Frozen Signals', '2018', 11, 1);
+SET @id_colecionavel = LAST_INSERT_ID();
+SET @id_gravadora = 3; 
+INSERT INTO Artista (nome) VALUES ('Artic Veins'); -- 8
+SET @id_artista = LAST_INSERT_ID();
+INSERT INTO Album (id_colecionavel, numero_catalogo, ean_upc, tipo_album, id_gravadora) 
+VALUES (@id_colecionavel, 'COL-CD-1834', '190758341223', 'CD', @id_gravadora);
+INSERT INTO Album_Artista (id_colecionavel, id_artista) 
+VALUES (@id_colecionavel, @id_artista);
+INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) 
+VALUES (1, @id_colecionavel, '2022-05-23');
+
+-- 21
+INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) 
+VALUES ('Album', 'Wildfire Heart', '2021', 13, 1);
+SET @id_colecionavel = LAST_INSERT_ID();
+INSERT INTO Gravadora (nome) VALUES ('Capitol Records'); -- 10
+SET @id_gravadora = LAST_INSERT_ID();
+INSERT INTO Artista (nome) VALUES ('Valerie Monroe'); -- 9
+SET @id_artista = LAST_INSERT_ID();
+INSERT INTO Album (id_colecionavel, numero_catalogo, ean_upc, tipo_album, id_gravadora) 
+VALUES (@id_colecionavel, 'CAP-CD-2147', '602438765432', 'CD', @id_gravadora);
+INSERT INTO Album_Artista (id_colecionavel, id_artista) 
+VALUES (@id_colecionavel, @id_artista);
+INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) 
+VALUES (1, @id_colecionavel, '2025-03-31');
