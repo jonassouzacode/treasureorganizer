3fade8037bd9675e0df9ca5b963e85716481a6a3
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 09:36:58 +0000

    colecionavel 20

--- inserir-colecionaveis-lista-desejos.sql
+++ inserir-colecionaveis-lista-desejos.sql
@@ -13,3 +13,15 @@ INSERT INTO Livros_Autor (isbn, id_autor) VALUES
 ('9780062458719', 8);
 INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) VALUES 
 (1, 19, '2026-01-17');
+
+--20
+INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) VALUES 
+('Album', 'Frozen Signals', '2018', 11, 1); 
+INSERT INTO Album (id_colecionavel, numero_catalogo, ean_upc, tipo_album, id_gravadora) VALUES 
+(20, 'COL-CD-1834', '190758341223', 'CD', 3); 
+INSERT INTO Artista (nome) VALUES
+('Artic Veins'); --9
+INSERT INTO Album_Artista (id_colecionavel, id_artista) VALUES 
+(20, 9); 
+INSERT INTO Lista_Desejos (id_usuario, id_colecionavel, data_adicao) VALUES 
+(1, 20, '2022-05-23');
