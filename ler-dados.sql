c6d50ce5cf38d340c487ffda5f5bc7ee8ca788b4
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 11:04:21 +0000

    ler livros da colecao

--- ler-dados.sql
+++ ler-dados.sql
@@ -1,1 +1,11 @@
 -- script de leitura de dados da colecao
+
+-- ler todos os livros da colecao
+
+SELECT Colecionavel.titulo 
+FROM Colecao  
+JOIN Colecionavel 
+ON Colecao.id_colecionavel = Colecionavel.id
+JOIN Livro  
+ON Livro.id_colecionavel = Colecionavel.id
+WHERE Colecao.id_usuario = 1;
