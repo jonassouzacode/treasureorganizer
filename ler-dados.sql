e032f9b81d01cd07f343c237f50b783eee7c2f76
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 11:04:52 +0000

    ler todos os albuns da colecao

--- ler-dados.sql
+++ ler-dados.sql
@@ -9,3 +9,14 @@ ON Colecao.id_colecionavel = Colecionavel.id
 JOIN Livro  
 ON Livro.id_colecionavel = Colecionavel.id
 WHERE Colecao.id_usuario = 1;
+
+-- ler todos os albuns da colecao
+
+SELECT * FROM Album;
+SELECT Colecionavel.titulo
+FROM Colecao
+JOIN Colecionavel
+ON Colecao.id_colecionavel = Colecionavel.id
+JOIN Album
+ON Album.id_colecionavel = Colecionavel.id
+WHERE Colecao.id_usuario = 1;
