3008b1e0d4a434ed989364558c2a40c87218b49a
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 11:09:32 +0000

    insere colecionavel da lista de desejos na colecao

--- alterar-dados.sql
+++ alterar-dados.sql
@@ -7,3 +7,8 @@ FROM Lista_Desejos
 JOIN Colecionavel
 ON Lista_Desejos.id_colecionavel = Colecionavel.id
 WHERE Lista_Desejos.id_usuario = 1;
+
+-- transferir colecionavel da lista de desejos poara a colecao
+
+INSERT INTO Colecao (id_usuario, id_colecionavel, data_aquisicao)
+VALUES (1, 19, CURDATE());
