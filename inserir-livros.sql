843300c82dbe129bb275b110217217f6f9cdd097
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 09:27:31 +0000

    colecionavel 1

--- inserir-livros.sql
+++ inserir-livros.sql
@@ -1,1 +1,15 @@
 --inserção dos livros colecionáveis à coleção 
+
+--1
+INSERT INTO Colecionavel (tipo_colecionavel, titulo, ano_lançamento, id_genero, id_idioma) VALUES 
+('Livro', 'Once Upon a Broken Heart', '2022', 2, 2);
+INSERT INTO Editora (nome) VALUES
+('Hodder & Stoughton'); --1
+INSERT INTO Autor (nome) VALUES
+('Stephanie Garber'); --1
+INSERT INTO Livro (id_colecionavel, isbn, id_tipo_encadernacao, id_editora) VALUES 
+(1, '9781529380941', 2, 1); 
+INSERT INTO Livros_Autor (isbn, id_autor) VALUES 
+('9781529380941', 1);
+INSERT INTO Colecao (id_usuario, id_colecionavel, data_aquisicao) VALUES 
+(1, 1, '2024-08-27'); 
