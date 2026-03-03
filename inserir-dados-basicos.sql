dd3aaf6af1bcf9012a0a20c501d0794559c0476f
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 13:56:17 +0000

    pequenos erros de sintaxe corrigidos no script de insercao de dados basicos

--- inserir-dados-basicos.sql
+++ inserir-dados-basicos.sql
@@ -1,58 +1,58 @@
 -- inserção de dados básicos:
 
---inserir usuario
+-- inserir usuario
 INSERT INTO Usuario (nome, email, senha) VALUES
 ('Jonas', 'admin', '1234');
 
---inserir idiomas
+--  idiomas
 INSERT INTO Idioma (idioma) VALUES
---1
+-- 1
 ('Português'),
---2
+-- 2
 ('Inglês'),
---3
+-- 3
 ('Espanhol'),
---4
+-- 4
 ('Francês'),
---5
+-- 5
 ('Japonês'),
---6
+-- 6
 ('Italiano');
 
---inserir generos
+-- inserir generos
 INSERT INTO Genero (genero) VALUES
---1
+-- 1
 ('Ficção Científica'),
---2
+-- 2
 ('Fantasia'),
---3
+-- 3
 ('Romance'),
---4
+-- 4
 ('Mistério'),
---5
+-- 5
 ('Terror'),
---6
+-- 6
 ('Aventura'),
---7
+-- 7
 ('Drama'),
---8
-('Suspense');
---9
-('Ficção');
---10
-('MPB');
---11
-('Rock Alternativo');
---12
-('Pop Alternativo');
---13
-('Pop');
---14
+-- 8
+('Suspense'),
+-- 9
+('Ficção'),
+-- 10
+('MPB'),
+-- 11
+('Rock Alternativo'),
+-- 12
+('Pop Alternativo'),
+-- 13
+('Pop'),
+-- 14
 ('Pop Psicodélico');
 
---inserir tipos encadernação
+-- inserir tipos encadernação
 INSERT INTO Encadernacao (nome, tipo_encadernacao) VALUES
---1
+-- 1
 ('Capa Dura', 'CD'),
---2
+-- 2
 ('Capa Mole', 'CM');
