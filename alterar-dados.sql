ec60920012bee195d1b8ac6827c9e91b547771ec
Jonas Souza <jonasmendes1301@gmail.com>
2026-03-03 11:09:04 +0000

    mostra lista de desejos antes de alterar

--- alterar-dados.sql
+++ alterar-dados.sql
@@ -1,1 +1,9 @@
 -- script de alteracao de dados
+
+-- mostrar lista de desejos
+
+SELECT Colecionavel.titulo 
+FROM Lista_Desejos
+JOIN Colecionavel
+ON Lista_Desejos.id_colecionavel = Colecionavel.id
+WHERE Lista_Desejos.id_usuario = 1;
